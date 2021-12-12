dbaeb64c850a479917f14d7a4abfe92bd78aa7c3
Michael Blume <michael.blume@currentsystem23.de>
2021-12-13 00:30:52 +0100

    Cleanup usings

--- EntityGenerator/EntityGeneratorTests/MicrosoftSqlServerReadTests.cs
+++ EntityGenerator/EntityGeneratorTests/MicrosoftSqlServerReadTests.cs
@@ -1,11 +1,6 @@
 ﻿using EntityGenerator.DatabaseObjects.DataAccessObjects;
 using EntityGenerator.Profile;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace EntityGeneratorTests
