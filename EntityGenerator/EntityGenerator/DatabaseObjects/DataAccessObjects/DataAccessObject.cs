8814b68acf356c524980dc9f459789c0332d795d
Michael Blume <michael.blume@currentsystem23.de>
2021-12-24 08:14:10 +0100

    Interface der abstrakten DataAccess Klasse hinzugefügt

--- EntityGenerator/EntityGenerator/DatabaseObjects/DataAccessObjects/DataAccessObject.cs
+++ EntityGenerator/EntityGenerator/DatabaseObjects/DataAccessObjects/DataAccessObject.cs
@@ -8,7 +8,7 @@ namespace EntityGenerator.DatabaseObjects.DataAccessObjects
   /// <summary>
   /// Abstract class <see cref="DataAccessObject"/> models the abstract technology independent data access for the source database.
   /// </summary>
-  public abstract class DataAccessObject
+  public abstract class DataAccessObject : IDataAccessObject
   {
     /// <summary>
     /// The profile data.
@@ -26,48 +26,37 @@ namespace EntityGenerator.DatabaseObjects.DataAccessObjects
       ProfileProvider = profileProvider;
     }
 
-    /// <summary>
-    /// The <see cref="ProfileProvider"/> for the current profile
-    /// </summary>
+    /// <inheritdoc />
     public ProfileProvider ProfileProvider { get; }
 
-    ///<summary>Determine the count of all database objects for the generator</summary>
-    ///<returns>the count of all database objects for the generator</returns>
+    /// <inheritdoc />
     public abstract int DatabaseObjectCount();
 
-    ///<summary>Determine the schemas of a database for the generator</summary>
-    ///<returns>the <see cref="List<SchemaDataTransferObject>"/> of all schemas in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<SchemaDto> DatabaseSchemas();
 
-    ///<summary>Determine the table value objects of a database for the generator</summary>
-    ///<returns>the <see cref="List<TableValueObjectDataTransferObject>"/> of all table value objects in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<TableValueObjectDto> DatabaseTableValueObjects();
 
-    ///<summary>Determine the columns of a database for the generator</summary>
-    ///<returns>the <see cref="List<ColumnDataTransferObject>"/> of all columns in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<ColumnDto> DatabaseColumns();
 
-    ///<summary>Determine the user defined functions of a database for the generator</summary>
-    ///<returns>the <see cref="List<FunctionDataTransferObject>"/> of all user defined functions in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<FunctionDto> DatabaseFunctions();
 
-    ///<summary>Determine the return column information for user defined functions of a database for the generator</summary>
+    /// <inheritdoc />
     public abstract void DatabaseFunctionReturnColumns(List<FunctionDto> databaseFunctions);
 
-    ///<summary>Determine the foreign keys of a database for the generator</summary>
-    ///<returns>the <see cref="List<ForeignKeyDto>"/> of all foreign keys in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<ForeignKeyDto> DatabaseForeignKeys();
 
-    ///<summary>Determine the check constraints of a database for the generator</summary>
-    ///<returns>the <see cref="List<CheckConstraintDto>"/> of all check constraints in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<CheckConstraintDto> DatabaseCheckConstraints();
 
-    ///<summary>Determine the indices of a database for the generator</summary>
-    ///<returns>the <see cref="List<IndexDto>"/> of all indices in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<IndexDto> DatabaseIndices();
 
-    ///<summary>Determine the triggers of a database for the generator</summary>
-    ///<returns>the <see cref="List<TriggerDto>"/> of all triggers in the database for the generator</returns>
+    /// <inheritdoc />
     public abstract List<TriggerDto> DatabaseTriggers();
   }
 }
---
+++ EntityGenerator/EntityGenerator/DatabaseObjects/DataAccessObjects/IDataAccessObject.cs
@@ -0,0 +1,58 @@
+﻿using EntityGenerator.DatabaseObjects.DataTransferObjects;
+using EntityGenerator.Profile;
+using System;
+using System.Collections.Generic;
+
+namespace EntityGenerator.DatabaseObjects.DataAccessObjects
+{
+  /// <summary>
+  /// Abstract class <see cref="DataAccessObject"/> models the abstract technology independent data access for the source database.
+  /// </summary>
+  public interface IDataAccessObject
+  {
+
+    /// <summary>
+    /// The <see cref="ProfileProvider"/> for the current profile
+    /// </summary>
+    ProfileProvider ProfileProvider { get; }
+
+    ///<summary>Determine the count of all database objects for the generator</summary>
+    ///<returns>the count of all database objects for the generator</returns>
+    int DatabaseObjectCount();
+
+    ///<summary>Determine the schemas of a database for the generator</summary>
+    ///<returns>the <see cref="List<SchemaDataTransferObject>"/> of all schemas in the database for the generator</returns>
+    List<SchemaDto> DatabaseSchemas();
+
+    ///<summary>Determine the table value objects of a database for the generator</summary>
+    ///<returns>the <see cref="List<TableValueObjectDataTransferObject>"/> of all table value objects in the database for the generator</returns>
+    List<TableValueObjectDto> DatabaseTableValueObjects();
+
+    ///<summary>Determine the columns of a database for the generator</summary>
+    ///<returns>the <see cref="List<ColumnDataTransferObject>"/> of all columns in the database for the generator</returns>
+    List<ColumnDto> DatabaseColumns();
+
+    ///<summary>Determine the user defined functions of a database for the generator</summary>
+    ///<returns>the <see cref="List<FunctionDataTransferObject>"/> of all user defined functions in the database for the generator</returns>
+    List<FunctionDto> DatabaseFunctions();
+
+    ///<summary>Determine the return column information for user defined functions of a database for the generator</summary>
+    void DatabaseFunctionReturnColumns(List<FunctionDto> databaseFunctions);
+
+    ///<summary>Determine the foreign keys of a database for the generator</summary>
+    ///<returns>the <see cref="List<ForeignKeyDto>"/> of all foreign keys in the database for the generator</returns>
+    List<ForeignKeyDto> DatabaseForeignKeys();
+
+    ///<summary>Determine the check constraints of a database for the generator</summary>
+    ///<returns>the <see cref="List<CheckConstraintDto>"/> of all check constraints in the database for the generator</returns>
+    List<CheckConstraintDto> DatabaseCheckConstraints();
+
+    ///<summary>Determine the indices of a database for the generator</summary>
+    ///<returns>the <see cref="List<IndexDto>"/> of all indices in the database for the generator</returns>
+    List<IndexDto> DatabaseIndices();
+
+    ///<summary>Determine the triggers of a database for the generator</summary>
+    ///<returns>the <see cref="List<TriggerDto>"/> of all triggers in the database for the generator</returns>
+    List<TriggerDto> DatabaseTriggers();
+  }
+}
