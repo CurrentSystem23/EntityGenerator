9d6e11228b973adcb2e2900eab7093fcea4e3930
Marcel Plumbohm <map@cs23.de>
2022-01-27 14:44:07 +0100

    Implement base language support

---
+++ EntityGenerator/EntityGenerator/Languages/TypeScript/TypeScriptLanguage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.Languages.TypeScript
+{
+  public class TypeScriptLanguage : LanguageBase
+  {
+    public override string Translate(in StringBuilder sb)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
