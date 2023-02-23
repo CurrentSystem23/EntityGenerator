7377fd20c678bd17798c3cf0b72a53d1fc9c77ae
Marcel Plumbohm <map@cs23.de>
2023-02-23 19:21:31 +0100

    Add Linter interface

---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Interfaces/ILinterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Interfaces
+{
+  public interface ILinterService
+  {
+    void ApplyIndentation(ref StringBuilder stringBuilder);
+  }
+}
---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Languages/.NET/CSharp/NETCSharpLinterService.cs
@@ -0,0 +1,17 @@
+﻿using EntityGenerator.CodeGeneration.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Languages.NET.CSharp
+{
+  public class NETCSharpLinterService : ILinterService
+  {
+    public void ApplyIndentation(ref StringBuilder stringBuilder)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
