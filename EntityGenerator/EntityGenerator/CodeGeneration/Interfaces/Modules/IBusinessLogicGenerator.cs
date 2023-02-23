f9ac5c8c740fbffee074389d5f1daebe30b79115
Marcel Plumbohm <map@cs23.de>
2023-02-23 19:20:40 +0100

    Add generator module interfaces

---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Interfaces/Modules/IBusinessLogicGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Interfaces.Modules
+{
+  public interface IBusinessLogicGenerator
+  {
+    void BuildTableInterface();
+    void BuildFunctionInterface();
+    void BuildTableValueFunctionInterface();
+    void BuildViewInterface();
+
+    void BuildTableClass();
+    void BuildFunctionClass();
+    void BuildTableValueFunctionClass();
+    void BuildViewClass();
+  }
+}
---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Interfaces/Modules/ICommonGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Interfaces.Modules
+{
+  public interface ICommonGenerator
+  {
+    void BuildBaseDTO();
+    void BuildTenantBase();
+    void BuildDatabaseDTOs();
+    void BuildSchemaDTO();
+  }
+}
