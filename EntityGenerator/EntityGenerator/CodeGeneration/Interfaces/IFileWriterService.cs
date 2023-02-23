50ede8393ac980c91ac989aef95213686f1422bb
Marcel Plumbohm <map@cs23.de>
2023-02-23 19:19:42 +0100

    Add Writer interfaces and empty implementation

---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Interfaces/IFileWriterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Interfaces
+{
+  internal interface IFileWriterService : IWriterService
+  {
+    public void WriteToFile(string data);
+  }
+}
---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Services/FileWriterService.cs
@@ -0,0 +1,22 @@
+﻿using EntityGenerator.CodeGeneration.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Services
+{
+  public class FileWriterService : IFileWriterService
+  {
+    public void Write(string data)
+    {
+      WriteToFile(data);
+    }
+
+    public void WriteToFile(string data)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
