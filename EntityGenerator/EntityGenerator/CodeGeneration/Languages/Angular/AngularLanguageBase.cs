ae427c0789f51be5b3e2255d191bdc1480297fda
Marcel Plumbohm <map@cs23.de>
2023-03-02 14:46:20 +0100

    WIP add TypeScript language features

---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Languages/Angular/AngularLanguageBase.cs
@@ -0,0 +1,17 @@
+﻿using EntityGenerator.Core.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Languages.Angular
+{
+  public abstract class AngularLanguageBase : LanguageBase
+  {
+    public override string GetDataType(DataTypes type)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Languages/Angular/TypeScript/AngularTypeScriptFrontend.cs
@@ -0,0 +1,55 @@
+﻿using EntityGenerator.CodeGeneration.Interfaces;
+using EntityGenerator.CodeGeneration.Interfaces.Modules;
+using EntityGenerator.Core.Models;
+using EntityGenerator.Profile.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Languages.Angular.TypeScript
+{
+  public abstract partial class AngularTypeScript : IFrontendGenerator
+  {
+    public StringBuilder BuildFunctionServiceHeader(ProfileGeneratorDto profile, Schema schema, Function function)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildFunctionServiceMethod(ProfileGeneratorDto profile, Schema schema, Function function, MethodType methodType)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildTableServiceHeader(ProfileGeneratorDto profile, Schema schema, Table table)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildTableServiceMethod(ProfileGeneratorDto profile, Schema schema, Table table, MethodType methodType)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildTableValueFunctionServiceHeader(ProfileGeneratorDto profile, Schema schema, TableValueFunction tableValueFunction)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildTableValueFunctionServiceMethod(ProfileGeneratorDto profile, Schema schema, TableValueFunction tableValueFunction, MethodType methodType)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildViewServiceHeader(ProfileGeneratorDto profile, Schema schema, View view)
+    {
+      throw new NotImplementedException();
+    }
+
+    public StringBuilder BuildViewServiceMethod(ProfileGeneratorDto profile, Schema schema, View view, MethodType methodType)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
