3e9088ee68b86e4da15d3469a6fbb3790b2a8a36
Marcel Plumbohm <map@cs23.de>
2023-07-26 21:12:55 +0200

    Introduce specialized generator base model

---
+++ EntityGenerator/EntityGenerator/CodeGeneration/Languages/.NET/CSharp/Model/GeneratorBaseModel.cs
@@ -0,0 +1,31 @@
+﻿using EntityGenerator.Core.Models.Enums;
+using EntityGenerator.Core.Models.ModelObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.CodeGeneration.Languages.NET.CSharp.Model
+{
+  internal class GeneratorBaseModel : BaseModel
+  {
+    public List<Column> Columns { get; } = new();
+
+    public List<Column> Parameters { get; }
+
+    public DataTypes ReturnType { get; }
+
+    public List<Column> ReturnTable { get; }
+
+    public GeneratorBaseModel(BaseModel baseModel)
+    {
+      foreach (PropertyInfo pi in baseModel.GetType().GetProperties())
+      {
+        PropertyInfo targetProp = this.GetType().GetProperty(pi.Name);
+        targetProp?.SetValue(this, pi.GetValue(baseModel, null), null);
+      }
+    }
+  }
+}
