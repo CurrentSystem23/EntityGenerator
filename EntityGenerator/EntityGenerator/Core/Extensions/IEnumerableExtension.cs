f76131d5cc818788fd71d2e16a6ca04eef4dbfac
Marcel Plumbohm <map@cs23.de>
2023-08-15 21:34:07 +0200

    Add IEnumerable extension function for empty enumerable objects

---
+++ EntityGenerator/EntityGenerator/Core/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator.Core.Extensions
+{
+  public static class IEnumerableExtensions
+  {
+    /// <summary>
+    /// Returns an empty IEnumerable object if the input object is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> source)
+    {
+      return source ?? Enumerable.Empty<T>();
+    }
+  }
+}
